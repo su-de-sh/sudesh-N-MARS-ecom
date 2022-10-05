3c48ba08ed11ad3a687c0cad8ebd94d45690e04d
su-de-sh <sudesh7443@gmail.com>
2022-10-05 11:47:11 +0545

    chore: console log the server address

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ const app = require("./app"); // the actual Express application
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
