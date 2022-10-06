d8076378f6c8087c54f14f5d92907fabcf36b3f7
su-de-sh <sudesh7443@gmail.com>
2022-10-06 19:55:00 +0545

    Chore: delete unnecessary file & add model produts

--- client/components/Router.js
+++ client/components/Router.js
@@ -1,12 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 
 import FrontPage from "./FrontPage";
-import MessageView from "./MessageView";
+// import MessageView from "./MessageView";
 
 const Router = () => (
   <Routes>
     <Route path="/" element={<FrontPage />} />
-    <Route path="/messages" element={<MessageView />} />
   </Routes>
 );
 
