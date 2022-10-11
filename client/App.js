65c4ee9fe96b3676980da297b2743308472f79c8
su-de-sh <sudesh7443@gmail.com>
2022-10-12 01:01:24 +0545

    feat: add product view in frontend

--- client/App.js
+++ client/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
+
 import Router from "./components/Router";
 
 const App = () => (
   <>
     <NavBar />
+
     <Router />
   </>
 );
---
+++ client/components/Products.js
@@ -0,0 +1,27 @@
+import React from "react";
+import productImage from "../assets/images/iphone.jpeg";
+
+const Products = () => {
+  const products = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return (
+    <div className="wrapper ">
+      <div className="  split-center"> All Products</div>
+
+      <div className="gallery">
+        {products.map((product, index) => {
+          return (
+            <div key={index} className="content">
+              <img className="product-img" src={productImage} alt="product" />
+              <h3 className="title">Iphone 14</h3>
+              <p className="desc">lorem ipsum a quick brown fox</p>
+              <h6 className="price">Rs. 8000</h6>
+              <button className="add-to-cart-btn"> Add to cart</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Products;
