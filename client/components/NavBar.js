857ac7e5a56b3d617151dbcc0ebd7bfc5be9bad8
su-de-sh <sudesh7443@gmail.com>
2022-10-11 21:13:20 +0545

    feat: add navbar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,21 +1,39 @@
 // import toskaLogo from "../assets/images/toskalogo_color.svg";
 import { Link } from "react-router-dom";
+import logo from "../assets/images/download.png";
+import searchIcon from "../assets/images/search-icon.png";
+import cartIcon from "../assets/images/cart.png";
 
 const NavBar = () => (
-  <div>NavBar</div>
-  // <div className="wrapper">
-  //   <div className="flex split-pair align-center region-tn">
-  //     <img src={toskaLogo} alt="toska" className="logo" />
-  //     <div className="flex gap-2">
-  //       <Link to="/" className="text-link">
-  //         Home
-  //       </Link>
-  //       <Link to="/messages" className="text-link">
-  //         Messages
-  //       </Link>
-  //     </div>
-  //   </div>
-  // </div>
+  <div className="wrapper">
+    <div className="flex split-pair align-center region-tn nav-bar">
+      <div className="flex  align-center gap-2">
+        <img src={logo} alt="toska" className="logo" />
+        <Link to="/" className="text-link">
+          Home
+        </Link>
+        <Link to="/category" className="text-link">
+          Category
+        </Link>
+        <div className="search-box flex align-center">
+          <form action="">
+            <input type="text" placeholder="Search" className="input-field" />
+          </form>
+          <img src={searchIcon} alt="search-icon" className="search-icon" />
+        </div>
+      </div>
+
+      <div className="flex align-center split-pair gap-1">
+        <Link to="/cart">
+          <img src={cartIcon} alt="cart-icon" className="cart-icon" />
+        </Link>
+
+        <Link to="/login" className="text-link ">
+          Login/Signup
+        </Link>
+      </div>
+    </div>
+  </div>
 );
 
 export default NavBar;
