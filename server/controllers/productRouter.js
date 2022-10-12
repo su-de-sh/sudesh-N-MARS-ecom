3a757875198801466dd9d0a48fccbf56c3dc4d2b
su-de-sh <sudesh7443@gmail.com>
2022-10-12 20:45:59 +0545

    feat: add limiting number of product feature

--- server/controllers/productRouter.js
+++ server/controllers/productRouter.js
@@ -3,7 +3,10 @@ const Product = require("../models/product");
 const productRouter = require("express").Router();
 
 productRouter.get("/", async (req, res) => {
-  const products = await Product.findAll({ attributes: {} });
+  const products = await Product.findAll({
+    attributes: {},
+    limit: req.query.limit,
+  });
   res.json(products);
 });
 
