8e8ecbb9e501083733e20792b84e0170a8960c78
su-de-sh <sudesh7443@gmail.com>
2022-10-22 23:42:03 +0545

    feat: add login forntend page

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <div className="wrapper container">
+        <div className="margin-sm  width-md container-md">
+          <form className="width-md text-center" action="">
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <p className="text-white margin-top-md">
+              Not registered? <Link to="/signup">SignUp</Link>
+            </p>
+            <button className="primary-button ">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
