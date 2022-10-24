0668a9badb367543db92b8cd86d099ad17d4d254
su-de-sh <sudesh7443@gmail.com>
2022-10-25 01:43:55 +0545

    feat: post request for placing order

--- client/components/Products.js
+++ client/components/Products.js
@@ -4,7 +4,9 @@ import { useSelector } from "react-redux";
 const Products = () => {
   //retrive all products from redux store
   const products = useSelector((state) => state.products);
-
+  const addToCart = (productId) => {
+    console.log(productId);
+  };
   return (
     <div className="wrapper ">
       <div className=" flex split-center"> All Products</div>
@@ -21,7 +23,13 @@ const Products = () => {
               <h3 className="title">{product.productName}</h3>
               <p className="desc">{product.specification.slice(0, 50)}...</p>
               <h6 className="price">Rs.{product.price}</h6>
-              <button className="add-to-cart-btn"> Add to cart</button>
+              <button
+                className="add-to-cart-btn"
+                onClick={() => addToCart(product.id)}
+              >
+                {" "}
+                Add to cart
+              </button>
             </div>
           );
         })}
