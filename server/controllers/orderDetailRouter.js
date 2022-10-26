4813ae87d4c8f8e670f0a532f1a0a7d3743dd08e
su-de-sh <sudesh7443@gmail.com>
2022-10-26 15:29:10 +0545

    fix: add try catch to handle error

--- server/controllers/orderDetailRouter.js
+++ server/controllers/orderDetailRouter.js
@@ -3,12 +3,16 @@ const { Product, OrderDetail, Order } = require("../models");
 const orderDetailRouter = require("express").Router();
 
 orderDetailRouter.get("/", async (req, res) => {
-  const orders = await OrderDetail.findAll({
-    include: [
-      { model: Product, attributes: ["productName", "price", "quantity"] },
-    ],
-  });
-  res.send(orders);
+  try {
+    const orders = await OrderDetail.findAll({
+      include: [
+        { model: Product, attributes: ["productName", "price", "quantity"] },
+      ],
+    });
+    res.send(orders);
+  } catch (err) {
+    res.send(err);
+  }
 });
 orderDetailRouter.get("/cart", async (req, res) => {
   try {
@@ -35,22 +39,26 @@ orderDetailRouter.get("/cart", async (req, res) => {
 });
 
 orderDetailRouter.post("/", async (req, res) => {
-  const pendingOrder = await Order.findOne({
-    where: {
-      status: "pending",
-    },
-  });
+  try {
+    const pendingOrder = await Order.findOne({
+      where: {
+        status: "pending",
+      },
+    });
 
-  if (pendingOrder) {
-    const order = { productId: req.body.productId, orderId: pendingOrder.id };
-    const response = await OrderDetail.create(order);
-    res.send(response);
-  } else {
-    const userId = req.user.id;
-    const newOrder = await Order.create({ userId });
-    const order = { productId: req.body.productId, orderId: newOrder.id };
-    const response = await OrderDetail.create(order);
-    res.json(response);
+    if (pendingOrder) {
+      const order = { productId: req.body.productId, orderId: pendingOrder.id };
+      const response = await OrderDetail.create(order);
+      res.send(response);
+    } else {
+      const userId = req.user.id;
+      const newOrder = await Order.create({ userId });
+      const order = { productId: req.body.productId, orderId: newOrder.id };
+      const response = await OrderDetail.create(order);
+      res.json(response);
+    }
+  } catch (err) {
+    res.send(err);
   }
 });
 
