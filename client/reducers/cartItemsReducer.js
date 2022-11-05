1671d2248a9d013825ef7a3da5783f96b5d4eb9b
su-de-sh <sudesh7443@gmail.com>
2022-11-05 18:32:44 +0545

    chore: refactor cartItemsreducer

--- client/reducers/cartItemsReducer.js
+++ client/reducers/cartItemsReducer.js
@@ -1,6 +1,10 @@
+//
+
 import { createSlice } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
 import orderServices from "../services/orderServices";
 import productServices from "../services/productServices";
+const user = JSON.parse(window.localStorage.getItem("loggedinUser"));
 
 const cartItemsSlice = createSlice({
   name: "cartItems",
@@ -17,81 +21,115 @@ const cartItemsSlice = createSlice({
   },
 });
 
-export const initializeCartItemsDatabase = () => {
+export const initializeCartItems = () => {
   return async (dispatch) => {
-    const itemsInCart = await orderServices.getCartItems();
-    // console.log(itemsInCart);
-    const itemsInCartLocal = JSON.parse(
-      window.localStorage.getItem("cartItems")
-    );
-    // console.log(itemsInCartLocal);
-    if (!itemsInCart.length && itemsInCartLocal.length) {
-      // console.log(itemsInCart);
-      // const promiseArray = itemsInCartLocal.map((item) =>
-      //   dispatch(addCartItemDatabase(item.id))
-      // );
-      // await Promise.all(promiseArray);
+    if (user) {
+      const itemsInCart = await orderServices.getCartItems(user);
 
-      dispatch(setItems(itemsInCartLocal));
-      // window.localStorage.setItem("cartItems", JSON.stringify([]));
-    } else if (!itemsInCart.length && !itemsInCartLocal.length) {
-      dispatch(setItems([]));
+      dispatch(setItems(itemsInCart));
     } else {
-      const cartItems = itemsInCart.map((item) => {
-        return { ...item.product, noOfProduct: item.quantity };
-      });
+      const itemsInCart = JSON.parse(window.localStorage.getItem("cartItems"));
+      if (!itemsInCart) {
+        window.localStorage.setItem("cartItems", JSON.stringify([]));
 
-      dispatch(setItems(cartItems));
+        dispatch(setItems([]));
+      } else {
+        dispatch(setItems(itemsInCart));
+      }
     }
+    // const itemsInCart = await orderServices.getCartItems();
+    // // console.log(itemsInCart);
+    // const itemsInCartLocal = JSON.parse(
+    //   window.localStorage.getItem("cartItems")
+    // );
+    // // console.log(itemsInCartLocal);
+    // if (!itemsInCart.length && itemsInCartLocal.length) {
+    //   // console.log(itemsInCart);
+    //   const promiseArray = itemsInCartLocal.map((item) =>
+    //     dispatch(addCartItemDatabase(item.id))
+    //   );
+    //   await Promise.all(promiseArray);
+
+    //   // dispatch(setItems(itemsInCartLocal));
+    //   // window.localStorage.setItem("cartItems", JSON.stringify([]));
+    // } else if (!itemsInCart.length && !itemsInCartLocal.length) {
+    //   dispatch(setItems([]));
+    // } else {
+    //   const cartItems = itemsInCart.map((item) => {
+    //     return { ...item.product, noOfProduct: item.quantity };
+    //   });
+
+    //   dispatch(setItems(cartItems));
+    // }
   };
 };
-export const initializeCartItemsLocal = () => {
-  return async (dispatch) => {
-    const itemsInCart = JSON.parse(window.localStorage.getItem("cartItems"));
-    // const cartItems = itemsInCart.map((item) => {
-    //   return { ...item};
-    // });
 
-    if (!itemsInCart) {
-      dispatch(setItems([]));
-      window.localStorage.setItem("cartItems", JSON.stringify([]));
-    }
-    // const cartItems = itemsInCart.map((item) => {
-    //   return { ...item.product, noOfProduct: item.quantity };
-    // });
-    // console.log(cartItems);
-    else {
+export const addItemToCart = (productId, quantity = 1) => {
+  return async (dispatch) => {
+    if (user) {
+      const order = await orderServices.createOrder(productId, quantity);
+      dispatch(addItem(order));
+    } else {
+      const itemsInCart = JSON.parse(window.localStorage.getItem("cartItems"));
+
+      const products = await productServices.getAll();
+
+      const order = products.find((product) => product.id === productId);
+      itemsInCart.push(order);
+      window.localStorage.setItem("cartItems", JSON.stringify(itemsInCart));
+
       dispatch(setItems(itemsInCart));
     }
   };
 };
-export const addCartItemDatabase = (productId) => {
-  return async (dispatch) => {
-    const order = await orderServices.createOrder(productId);
-    const products = await orderServices.getAll();
 
-    const addedItem = products.find(
-      (product) => product.productId === order.productId
-    );
+// export const initializeCartItemsLocal = () => {
+//   return async (dispatch) => {
+//     const itemsInCart = JSON.parse(window.localStorage.getItem("cartItems"));
+//     // const cartItems = itemsInCart.map((item) => {
+//     //   return { ...item};
+//     // });
 
-    dispatch(addItem({ ...addedItem.product, noOfProduct: 1 }));
-  };
-};
+//     if (!itemsInCart) {
+//       dispatch(setItems([]));
+//       window.localStorage.setItem("cartItems", JSON.stringify([]));
+//     }
+//     // const cartItems = itemsInCart.map((item) => {
+//     //   return { ...item.product, noOfProduct: item.quantity };
+//     // });
+//     // console.log(cartItems);
+//     else {
+//       dispatch(setItems(itemsInCart));
+//     }
+//   };
+// };
+// export const addCartItemDatabase = (productId) => {
+//   return async (dispatch) => {
+//     const order = await orderServices.createOrder(productId);
+//     const products = await productServices.getAll();
 
-export const addCartItemLocal = (productId) => {
-  return async (dispatch) => {
-    const products = await productServices.getAll();
-    // console.log(productId, products);
-    const addedItem = products.find((product) => product.id === productId);
+//     const addedItem = products.find(
+//       (product) => product.productId === order.productId
+//     );
 
-    dispatch(addItem({ ...addedItem, noOfProduct: 1 }));
-    // dispatch(addItem(addedItem));
+//     dispatch(addItem({ ...addedItem.product, noOfProduct: 1 }));
+//   };
+// };
 
-    const itemsInCart = JSON.parse(window.localStorage.getItem("cartItems"));
-    itemsInCart.push({ ...addedItem, noOfProduct: 1 });
-    window.localStorage.setItem("cartItems", JSON.stringify(itemsInCart));
-  };
-};
+// export const addCartItemLocal = (productId) => {
+//   return async (dispatch) => {
+//     const products = await productServices.getAll();
+//     // console.log(productId, products);
+//     const addedItem = products.find((product) => product.id === productId);
+
+//     dispatch(addItem({ ...addedItem, noOfProduct: 1 }));
+//     // dispatch(addItem(addedItem));
+
+//     const itemsInCart = JSON.parse(window.localStorage.getItem("cartItems"));
+//     itemsInCart.push({ ...addedItem, noOfProduct: 1 });
+//     window.localStorage.setItem("cartItems", JSON.stringify(itemsInCart));
+//   };
+// };
 
 export const { setItems, addItem } = cartItemsSlice.actions;
 export default cartItemsSlice.reducer;
